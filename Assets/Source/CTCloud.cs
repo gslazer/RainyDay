0f1f1f0f5823fae60763f7df5c1a8191948130fd
Gslazer <gslazer2@gmail.com>
2015-03-10 00:28:28 +0900

    Thunder Storming!!!!
    번개구름/번개 오브젝트 생성루틴 제작.
    구조 약간 수정.

---
+++ Assets/Source/CTCloud.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class CTCloud : MonoBehaviour {
+	float deltaTime=0.0f;
+	bool notActedYet;
+	SpriteRenderer spriteRenderer; 
+	public Sprite afterSprite;
+	public GameObject thunder;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>(); 
+		Destroy (gameObject, 1.0f);  
+		notActedYet = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deltaTime += Time.deltaTime;
+		if (deltaTime > 0.5f && notActedYet) {
+			Instantiate (thunder, this.transform.position + new Vector3 (0, -5.0f, 0.5f), Quaternion.Euler (0, 180, 0));
+			spriteRenderer.sprite = afterSprite;
+			notActedYet=false;
+		}
+	}
+}
---
+++ Assets/Source/CThunder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CThunder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 0.5f);  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
