2fd81e60d2f18ecb45d3dac96d636ab984249404
Gslazer <gslazer2@gmail.com>
2015-03-10 00:29:56 +0900

    누락된 commit
    제곧내

--- Assets/Source/CTGameObject.cs
+++ Assets/Source/CTGameObject.cs
@@ -4,7 +4,11 @@ using AssemblyCSharp;
 
 public class CTGameObject : MonoBehaviour {
 	public GameObject cloud;
+	public GameObject tCloud;
 	float deltaTime;
+	float cloudTime;
+	float tCloudTime;
+	float nextTCloudTime=5.0f;
 	// Use this for initialization
 	void Start () {
 		generateCloud();
@@ -20,20 +24,39 @@ public class CTGameObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		InputProcess ();
-		setDeltaTime();
-		deltaTime+=ToolManager.deltaTime;
-		if(deltaTime>0.1){
+		upTimer ();
+
+		if(cloudTime>0.1f){
 			generateCloud();
-			deltaTime=0;
+			cloudTime=0;
+		}
+		if (tCloudTime > nextTCloudTime) {
+			generateTCloud();
+			tCloudTime=0;
+			nextTCloudTime=Random.Range (0.5f,5.0f);
 		}
 	}
-	
+
+	private void upTimer(){
+		setDeltaTime();
+		deltaTime=ToolManager.deltaTime;
+		cloudTime += deltaTime;
+		tCloudTime += deltaTime;
+	}
+
 	public void generateCloud(){
 		Vector3 position;
 		float x=Random.Range(-3.5f,3.5f);
 		float y=5;
 		position=new Vector3(x,y,0);
 		Instantiate(cloud,position,Quaternion.Euler(0,180,0));
+	}
+	public void generateTCloud(){
+		Vector3 position;
+		float x=Random.Range(-3.5f,3.5f);
+		float y=5;
+		position=new Vector3(x,y,-1);
+		Instantiate(tCloud,position,Quaternion.Euler(0,180,0));
 	}
 
 	public void InputProcess(){
